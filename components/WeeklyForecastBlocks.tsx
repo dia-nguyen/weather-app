ce0ea5ea2843a94bfd8f9a5df28acb0182b72b08
Diane <hello@dianenguyen.ca>
2024-01-12 15:44:50 -0800

    Updated weather icons

--- components/WeeklyForecastBlocks.tsx
+++ components/WeeklyForecastBlocks.tsx
@@ -17,13 +17,12 @@ export default function WeeklyForecast() {
 
   if (!isLoading) {
     const forecast = weather.daily;
-
     return (
-      <div className="grid grid-cols-6 border rounded-lg  border-[rgba(255,255,255,0.2)]">
+      <div className="grid grid-cols-6 border rounded  border-[rgba(255,255,255,0.2)]">
         {forecast.map((day: DailyWeatherProps, index: any) => (
           index >= 1 && index <= 6 && (
-            <div key={index} className={`p-2 text-center ${index == 1 ? "bg-[rgba(255,255,255,0.2)]" : ""}`}>
-              <Image src={`https://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`} width={30} height={30} alt={day.weather[0].description} />
+            <div key={index} className={`p-2 text-center ${index == 1 ? "bg-[rgba(255,255,255,0.2)] rounded-l" : ""}`}>
+              <Image src={`/assets/weather-icons/${day.weather[0].icon}.svg`} width={30} height={30} alt={day.weather[0].description} className="invert" />
               <p>{formatDayOfWeek(day.dt)}</p>
               <p>{convertUnit(unit, day.temp.dayTime, "temp")}°</p>
             </div>)
@@ -35,9 +34,9 @@ export default function WeeklyForecast() {
 
 function ForecastSkeleton() {
   return (
-    <div className="grid grid-cols-6 border rounded-lg  border-[rgba(255,255,255,0.2)]">
+    <div className="grid grid-cols-6 border rounded  border-[rgba(255,255,255,0.2)]">
       {Array.from(Array(6), (_, i) => <div className={`p-2 text-center ${i == 0 ? "bg-[rgba(255,255,255,0.2)]" : ""}`} key={i}>
-        <span className="relative w-[25px] inline-block before:rounded before:content-[''] before:absolute before:top-0 before:w-full before:left-0 before:bg-[rgba(255,255,255,0.4)] before:backdrop-blur-sm before:h-full"><Image src={`/assets/weather-icons/1001.svg`} width={30} height={30} alt="preview" className="relative opacity-0" /></span>
+        <span className="relative w-[25px] inline-block before:rounded before:content-[''] before:absolute before:top-0 before:w-full before:left-0 before:bg-[rgba(255,255,255,0.4)] before:backdrop-blur-sm before:h-full"><Image src={`/assets/weather-icons/01d.svg`} width={30} height={30} alt="preview" className="relative opacity-0" /></span>
         <p className="text-transparent w-full relative before:rounded before:content-[''] before:absolute before:top-[15%] before:w-full before:left-0 before:bg-[rgba(255,255,255,0.4)] before:backdrop-blur-sm before:h-[75%]">Day</p>
         <p className="text-transparent w-full relative before:rounded before:content-[''] before:absolute before:top-[15%] before:w-full before:left-0 before:bg-[rgba(255,255,255,0.4)] before:backdrop-blur-sm before:h-[75%]">Temp</p>
       </div>)}
